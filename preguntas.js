e410df7392e85905bde6ad23e80eb45be3bdbf32
MatiGaete2023 <154522773+MatiGaete2023@users.noreply.github.com>
2025-04-15 19:52:34 -0400

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,28 @@
+
+const preguntas_multiples = [
+    {
+        tipo: "Seleccion multiple",
+        pregunta: "¿Cuál de los siguientes es un elemento esencial del acto jurídico?",
+        opciones: ["Condición", "Objeto lícito", "Plazo", "Modalidad"],
+        respuesta: "B"
+    },
+    {
+        tipo: "Seleccion multiple",
+        pregunta: "El silencio como manifestación de voluntad en el acto jurídico...",
+        opciones: ["Siempre se considera aceptación", "Nunca produce efectos", "Puede ser relevante si hay norma o costumbre", "Anula el consentimiento"],
+        respuesta: "C"
+    }
+];
+
+const preguntas_vf = [
+    {
+        tipo: "Verdadero / Falso",
+        pregunta: "El error esencial en el objeto puede anular un acto jurídico.",
+        respuesta: "Verdadero"
+    },
+    {
+        tipo: "Verdadero / Falso",
+        pregunta: "El acto jurídico unilateral no existe en el derecho chileno.",
+        respuesta: "Falso"
+    }
+];
